e4264635dcbb7467c64d532c547f4c95370bace9
Sundar Nadathur <sundar.nadathur@intel.com>
2018-11-18 21:30:30 -0800

    Devstack enablement for OPAE FPGA driver.
    This is partial refactor of https://review.openstack.org/#/c/584170
    based on review comments.
    
    Change-Id: Ia25df018ceae84c613f2f5dea1a6c3d175b4a551
    Storyboard: https://storyboard.openstack.org/#!/story/2004250

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -6,46 +6,49 @@
 
 echo_summary "cyborg devstack plugin.sh called: $1/$2"
 source $DEST/cyborg/devstack/lib/cyborg
+source $DEST/cyborg/devstack/lib/opae
 
 if is_service_enabled cyborg-api cyborg-cond; then
     if [[ "$1" == "stack" ]]; then
-        if [[ "$2" == "install" ]]; then
+        if [[ "$2" == "pre-install" ]]; then
+        # stack/pre-install - Called after (OS) setup is complete and before
+        # project source is installed
+            echo_summary "Installing additional Cyborg packages"
+            if install_opae_packages; then
+               echo_summary "INFO: Additional Cyborg packages installed"
+            else
+               echo "WARNING: Failed to install additional Cyborg packages"
+            fi
+        elif [[ "$2" == "install" ]]; then
         # stack/install - Called after the layer 1 and 2 projects source and
         # their dependencies have been installed
-
             echo_summary "Installing Cyborg"
             if ! is_service_enabled nova; then
                 source $RC_DIR/lib/nova_plugins/functions-libvirt
                 install_libvirt
             fi
             install_cyborg
-
         elif [[ "$2" == "post-config" ]]; then
         # stack/post-config - Called after the layer 1 and 2 services have been
         # configured. All configuration files for enabled services should exist
         # at this point.
-
             echo_summary "Configuring Cyborg"
             configure_cyborg
             create_cyborg_accounts
-
         elif [[ "$2" == "extra" ]]; then
         # stack/extra - Called near the end after layer 1 and 2 services have
         # been started.
-
             # Initialize cyborg
             init_cyborg
 
             # Start the cyborg API and cyborg taskmgr components
             echo_summary "Starting Cyborg"
             start_cyborg
-
         fi
     fi
 
     if [[ "$1" == "unstack" ]]; then
     # unstack - Called by unstack.sh before other services are shut down.
-
         stop_cyborg
     fi
 
@@ -53,6 +56,6 @@ if is_service_enabled cyborg-api cyborg-cond; then
     # clean - Called by clean.sh before other services are cleaned, but after
     # unstack.sh has been called.
         cleanup_cyborg
+        uninstall_opae_packages
     fi
 fi
-
