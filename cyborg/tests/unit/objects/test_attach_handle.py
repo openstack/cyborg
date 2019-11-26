7c7dba5d48c24693db71dc7c08c12e1626e9176b
chenke <chen.ke14@zte.com.cn>
2019-11-26 14:51:02 +0800

    Improve objects/attach_handle.py UT coverage from 68% to 100%
    Change-Id: I92da95d1f63d879893de295dbc7aba03b6b5d51d

--- cyborg/tests/unit/objects/test_attach_handle.py
+++ cyborg/tests/unit/objects/test_attach_handle.py
@@ -44,6 +44,40 @@ class TestAttachHandleObject(base.DbTestCase):
             mock_attach_handle_get.assert_called_once_with(self.context, id)
             self.assertEqual(self.context, attach_handle._context)
 
+    def test_get_attach_handle_by_deployable_id(self):
+        deployable_id = self.fake_attach_handle['deployable_id']
+        ah_filter = {'deployable_id': deployable_id}
+        with mock.patch.object(objects.AttachHandle, 'list',
+                               autospec=True) as mock_attach_handle_list:
+            mock_attach_handle_list.return_value = [self.fake_attach_handle]
+            attach_handles = objects.AttachHandle.get_ah_list_by_deployable_id(
+                self.context, deployable_id)
+            mock_attach_handle_list.assert_called_once_with(
+                self.context, ah_filter)
+            self.assertEqual(
+                deployable_id,
+                attach_handles[0]['deployable_id'])
+
+    def test_get_attach_handle_by_depid_attachinfo(self):
+        deployable_id = self.fake_attach_handle['deployable_id']
+        attach_info = self.fake_attach_handle['attach_info']
+        ah_filter = {'deployable_id': deployable_id,
+                     'attach_info': attach_info}
+        with mock.patch.object(objects.AttachHandle, 'list',
+                               autospec=True) as mock_attach_handle_list:
+            mock_attach_handle_list.return_value = [self.fake_attach_handle]
+            attach_handles = objects.AttachHandle.get_ah_by_depid_attachinfo(
+                self.context, deployable_id, attach_info)
+            mock_attach_handle_list.assert_called_once_with(
+                self.context, ah_filter)
+            self.assertEqual(attach_info, attach_handles['attach_info'])
+
+            # test objects.AttachHandle.list() return []
+            mock_attach_handle_list.return_value = []
+            attach_handle = objects.AttachHandle.get_ah_by_depid_attachinfo(
+                self.context, deployable_id, attach_info)
+            self.assertIsNone(attach_handle)
+
     def test_list(self):
         with mock.patch.object(self.dbapi, 'attach_handle_list',
                                autospec=True) as mock_attach_handle_list:
@@ -53,6 +87,46 @@ class TestAttachHandleObject(base.DbTestCase):
             self.assertEqual(1, len(attach_handles))
             self.assertIsInstance(attach_handles[0], objects.AttachHandle)
             self.assertEqual(self.context, attach_handles[0]._context)
+
+    def test_list_with_filter(self):
+        with mock.patch.object(self.dbapi, 'attach_handle_get_by_filters',
+                               autospec=True) as mock_ah_with_filter_list:
+            mock_ah_with_filter_list.return_value = [self.fake_attach_handle]
+            filters = {'limit': 1}
+            attach_handles = objects.AttachHandle.list(self.context, filters)
+            self.assertEqual(1, mock_ah_with_filter_list.call_count)
+            self.assertEqual(1, len(attach_handles))
+            self.assertIsInstance(attach_handles[0], objects.AttachHandle)
+            mock_ah_with_filter_list.assert_called_once_with(
+                self.context,
+                {},
+                sort_dir='desc',
+                sort_key='created_at',
+                limit=1,
+                marker=None,
+                )
+
+    @mock.patch.object(objects.base.CyborgObject, "_from_db_object")
+    def test_attach_handle_allocate(self, mock_from_db_obj):
+        deployable_id = self.fake_attach_handle['deployable_id']
+        with mock.patch.object(self.dbapi, 'attach_handle_allocate',
+                               autospec=True) as mock_ah_allocate:
+            mock_ah_allocate.return_value = self.fake_attach_handle
+            objects.AttachHandle.allocate(self.context, deployable_id)
+            mock_from_db_obj.assert_called_once_with(
+                mock.ANY,
+                self.fake_attach_handle)
+
+    def test_attach_handle_deallocate(self):
+        attach_handle_uuid = self.fake_attach_handle['uuid']
+        with mock.patch.object(self.dbapi, 'attach_handle_update',
+                               autospec=True) as mock_ah_update:
+            ah_obj = objects.AttachHandle(**self.fake_attach_handle)
+            ah_obj.deallocate(self.context)
+            mock_ah_update.assert_called_once_with(
+                self.context,
+                attach_handle_uuid,
+                {"in_use": False})
 
     def test_create(self):
         with mock.patch.object(self.dbapi, 'attach_handle_create',
