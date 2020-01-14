6b28ca2722885f4eb4e0ae40b26b19d2347e21c0
chenke <chen.ke14@zte.com.cn>
2020-01-14 11:30:32 +0800

    Improve UT for cyborg/db device profile
    This patch add some UT for device profile:
    1. get
    2. update
    3. create
    4. list
    5. delete
    
    Change-Id: Ia6f415b74150cdda94d71dc8b5e760f7758a526e
    Story: 2007091
    Task: 38120

--- cyborg/tests/unit/db/test_db_device_profile.py
+++ cyborg/tests/unit/db/test_db_device_profile.py
@@ -21,15 +21,79 @@ from oslo_utils import uuidutils
 
 from cyborg.common import exception
 from cyborg.tests.unit.db import base
+from cyborg.tests.unit.db import utils
 
 
-class TestDbDeviceProfileTestCase(base.DbTestCase):
+class TestDbDeviceProfile(base.DbTestCase):
+
+    def test_get_by_uuid(self):
+        created_dp = utils.create_test_device_profile(self.context)
+        queried_dp = self.dbapi.device_profile_get_by_uuid(
+            self.context, created_dp['uuid'])
+        self.assertEqual(created_dp['uuid'], queried_dp['uuid'])
+
+    def test_get_by_id(self):
+        created_dp = utils.create_test_device_profile(self.context)
+        queried_dp = self.dbapi.device_profile_get_by_id(
+            self.context, created_dp['id'])
+        self.assertEqual(created_dp['id'], queried_dp['id'])
+
+    def test_update(self):
+        created_dp = utils.create_test_device_profile(self.context)
+        queried_dp = self.dbapi.device_profile_update(
+            self.context, created_dp['uuid'], {'name': 'updated_name'})
+        self.assertEqual('updated_name', queried_dp['name'])
+
+    def test_list(self):
+        uuids = []
+        for i in range(1, 4):
+            dp = utils.create_test_device_profile(
+                self.context,
+                id=i,
+                uuid=uuidutils.generate_uuid(),
+                name="device_profile_name_%s" % i)
+            uuids.append(dp['uuid'])
+        dps = self.dbapi.device_profile_list(self.context)
+        dp_uuids = [item.uuid for item in dps]
+        self.assertEqual(sorted(uuids), sorted(dp_uuids))
+
+    def test_list_filter_by_name(self):
+        utils.create_test_device_profile(
+            self.context,
+            id=1,
+            uuid=uuidutils.generate_uuid(),
+            name="name_1")
+        utils.create_test_device_profile(
+            self.context,
+            id=2,
+            uuid=uuidutils.generate_uuid(),
+            name="name_2")
+        res = self.dbapi.device_profile_list_by_filters(
+            self.context, filters={"name": "name_1"})
+        self.assertEqual(1, len(res))
+        self.assertEqual('name_1', res[0]['name'])
+
+    def test_delete(self):
+        created_dp = utils.create_test_device_profile(self.context)
+        return_value = self.dbapi.device_profile_delete(
+            self.context,
+            created_dp['uuid'])
+        self.assertIsNone(return_value)
 
     def test_get_by_uuid_not_exist(self):
         random_uuid = uuidutils.generate_uuid()
         self.assertRaises(exception.ResourceNotFound,
                           self.dbapi.device_profile_get_by_uuid,
                           self.context, random_uuid)
+
+    def test_update_with_uuid_not_exist(self):
+        utils.create_test_device_profile(self.context)
+        random_uuid = uuidutils.generate_uuid()
+        self.assertRaises(exception.ResourceNotFound,
+                          self.dbapi.device_profile_update,
+                          self.context,
+                          random_uuid,
+                          {'name': 'updated_name'})
 
     def test_get_by_id_not_exist(self):
         fake_id = sys.maxsize
