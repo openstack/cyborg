e2e1e3f156d556becfa9cd5edf2bdf0ebb8f9214
chenke <chen.ke14@zte.com.cn>
2020-01-16 16:39:12 +0800

    Improve UT for cyborg/db attach handle
    This patch add some UT for attach handle:
    1. get
    2. update
    3. create
    4. list
    5. delete
    6. allocate
    
    Change-Id: I5e683c99d1e08ed6a166a110a87b665cdbc5bde3
    Story: 2007091
    Task: 38161

--- cyborg/tests/unit/db/test_db_attach_handle.py
+++ cyborg/tests/unit/db/test_db_attach_handle.py
@@ -19,9 +19,109 @@ from oslo_utils import uuidutils
 
 from cyborg.common import exception
 from cyborg.tests.unit.db import base
+from cyborg.tests.unit.db import utils
 
 
-class TestDbAttachHandleTestCase(base.DbTestCase):
+class TestDbAttachHandle(base.DbTestCase):
+
+    def test_create(self):
+        random_uuid = uuidutils.generate_uuid()
+        kw = {'uuid': random_uuid}
+        created_ah = utils.create_test_attach_handle(self.context, **kw)
+        self.assertEqual(random_uuid, created_ah['uuid'])
+
+    def test_get_by_uuid(self):
+        created_ah = utils.create_test_attach_handle(self.context)
+        queried_ah = self.dbapi.attach_handle_get_by_uuid(
+            self.context, created_ah['uuid'])
+        self.assertEqual(created_ah['uuid'], queried_ah['uuid'])
+
+    def test_get_by_id(self):
+        created_ah = utils.create_test_attach_handle(self.context)
+        queried_ah = self.dbapi.attach_handle_get_by_id(
+            self.context, created_ah['id'])
+        self.assertEqual(created_ah['id'], queried_ah['id'])
+
+    def test_update(self):
+        created_ah = utils.create_test_attach_handle(self.context)
+        queried_ah = self.dbapi.attach_handle_update(
+            self.context, created_ah['uuid'], {'attach_type': 'TEST_PCI'})
+        self.assertEqual('TEST_PCI', queried_ah['attach_type'])
+
+    def test_list(self):
+        uuids = []
+        for i in range(1, 4):
+            ah = utils.create_test_attach_handle(
+                self.context,
+                id=i,
+                uuid=uuidutils.generate_uuid())
+            uuids.append(ah['uuid'])
+        ahs = self.dbapi.attach_handle_list(self.context)
+        ah_uuids = [item.uuid for item in ahs]
+        self.assertEqual(sorted(uuids), sorted(ah_uuids))
+
+    def test_list_by_type(self):
+        ah1 = utils.create_test_attach_handle(
+            self.context,
+            id=1,
+            uuid=uuidutils.generate_uuid(),
+            attach_type='PCI')
+        utils.create_test_attach_handle(
+            self.context,
+            id=2,
+            uuid=uuidutils.generate_uuid(),
+            attach_type='TEST_PCI')
+        res = self.dbapi.attach_handle_list_by_type(
+            self.context, attach_type='PCI')
+        self.assertEqual(1, len(res))
+        self.assertEqual(ah1['uuid'], res[0]['uuid'])
+
+    def test_get_by_filters(self):
+        ah1 = utils.create_test_attach_handle(
+            self.context,
+            id=1,
+            uuid=uuidutils.generate_uuid(),
+            deployable_id=1)
+        utils.create_test_attach_handle(
+            self.context,
+            id=2,
+            uuid=uuidutils.generate_uuid(),
+            deployable_id=2)
+        res = self.dbapi.attach_handle_get_by_filters(
+            self.context, filters={"deployable_id": 1})
+        self.assertEqual(1, len(res))
+        self.assertEqual(ah1['uuid'], res[0]['uuid'])
+
+    def test_allocate(self):
+        utils.create_test_attach_handle(
+            self.context,
+            id=1,
+            uuid=uuidutils.generate_uuid(),
+            deployable_id=1)
+        allocate_ah = self.dbapi.attach_handle_allocate(
+            self.context, deployable_id=1)
+        self.assertTrue(allocate_ah['in_use'])
+
+    def test_delete(self):
+        created_ah = utils.create_test_attach_handle(self.context)
+        return_value = self.dbapi.attach_handle_delete(
+            self.context,
+            created_ah['uuid'])
+        self.assertIsNone(return_value)
+
+    def test_list_filter_is_none(self):
+        """The main test is filters=None. If filters=None,
+        it will be initialized to {}, that will return all attach
+        handle same as the List Attach Handle API response.
+        """
+        ah1 = utils.create_test_attach_handle(
+            self.context,
+            id=1,
+            uuid=uuidutils.generate_uuid())
+        res = self.dbapi.attach_handle_get_by_filters(
+            self.context, filters=None)
+        self.assertEqual(1, len(res))
+        self.assertEqual(ah1['uuid'], res[0]['uuid'])
 
     def test_get_by_uuid_not_exist(self):
         random_uuid = uuidutils.generate_uuid()
