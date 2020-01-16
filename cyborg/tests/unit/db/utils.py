d8cbe092deaae6be2d90221ce64b40bffa3116c6
chenke <chen.ke14@zte.com.cn>
2020-01-16 14:53:00 +0800

    Define fake db objects for UT
    This is a series of unit test optimizations for db.
    
    Change-Id: I96a2f8292c7ada1508af32d5b56d746b36abe054
    Story: 2007091
    Task: 38140

--- cyborg/tests/unit/db/utils.py
+++ cyborg/tests/unit/db/utils.py
@@ -15,6 +15,8 @@
 
 """Cyborg db test utilities."""
 
+from cyborg.db import api as db_api
+
 
 def get_test_deployable(**kw):
     return {
@@ -28,6 +30,51 @@ def get_test_deployable(**kw):
         'created_at': kw.get('created_at', None),
         'updated_at': kw.get('updated_at', None)
     }
+
+
+def create_test_deployable(context, **kwargs):
+    """Create test deployable entry in DB and return Deployable DB object.
+
+    Function to be used to create test Deployable objects in the database.
+    :param context: request context.
+    :param kwargs: kwargs which override default deployable attributes.
+    :returns: Test Deployable DB object.
+    """
+    deployable = get_test_deployable(**kwargs)
+    dbapi = db_api.get_instance()
+    return dbapi.deployable_create(context, deployable)
+
+
+def get_test_device(**kw):
+    std_board_info = {
+        "class": "Fake class",
+        "device_id": "0xabcd"
+        }
+    return {
+        'id': kw.get('id', 1),
+        'uuid': kw.get('uuid', '20efe63d-dfea-4a37-ad94-4116fba5012'),
+        'type': kw.get('type', "FPGA"),
+        'vendor': kw.get('vendor', "0xABCD"),
+        'model': kw.get('model', 'miss model info'),
+        'std_board_info': kw.get('std_board_info', std_board_info),
+        'vendor_board_info': kw.get('vendor_board_info', 'fake_vendor_info'),
+        'hostname': kw.get('hostname', "localhost"),
+        'created_at': kw.get('created_at', None),
+        'updated_at': kw.get('updated_at', None),
+    }
+
+
+def create_test_device(context, **kwargs):
+    """Create test device entry in DB and return Device DB object.
+
+    Function to be used to create test Device objects in the database.
+    :param context: request context.
+    :param kwargs: kwargs which override default device attributes.
+    :returns: Test Device DB object.
+    """
+    device = get_test_device(**kwargs)
+    dbapi = db_api.get_instance()
+    return dbapi.device_create(context, device)
 
 
 def get_test_extarq(**kwargs):
@@ -46,6 +93,19 @@ def get_test_extarq(**kwargs):
         'created_at': kwargs.get('created_at', None),
         'updated_at': kwargs.get('updated_at', None)
     }
+
+
+def create_test_extarq(context, **kwargs):
+    """Create test extend arq entry in DB and return ExtArq DB object.
+
+    Function to be used to create test ExtArq objects in the database.
+    :param context: request context.
+    :param kwargs: kwargs which override default extend arq attributes.
+    :returns: Test ExtArq DB object.
+    """
+    ext_arq = get_test_extarq(**kwargs)
+    dbapi = db_api.get_instance()
+    return dbapi.extarq_create(context, ext_arq)
 
 
 def get_test_arq(**kwargs):
@@ -81,6 +141,20 @@ def get_test_attach_handle(**kw):
     }
 
 
+def create_test_attach_handle(context, **kwargs):
+    """Create test attach handle entry in DB and return
+    AttachHandle DB object.
+
+    Function to be used to create test AttachHandle objects in the database.
+    :param context: request context.
+    :param kwargs: kwargs which override default attach handle attributes.
+    :returns: Test AttachHandle DB object.
+    """
+    attach_handle = get_test_attach_handle(**kwargs)
+    dbapi = db_api.get_instance()
+    return dbapi.attach_handle_create(context, attach_handle)
+
+
 def get_test_control_path(**kw):
     return {
         'uuid': kw.get('uuid', '10efe63d-dfea-4a37-ad94-4116fba50986'),
@@ -109,3 +183,17 @@ def get_test_device_profile(**kw):
         'created_at': kw.get('create_at', None),
         'updated_at': kw.get('updated_at', None),
     }
+
+
+def create_test_device_profile(context, **kwargs):
+    """Create test device profile entry in DB and return
+    DeviceProfile DB object.
+
+    Function to be used to create test DeviceProfile objects in the database.
+    :param context: request context.
+    :param kwargs: kwargs which override default device profile attributes.
+    :returns: Test DeviceProfile DB object.
+    """
+    device_profile = get_test_device_profile(**kwargs)
+    dbapi = db_api.get_instance()
+    return dbapi.device_profile_create(context, device_profile)
