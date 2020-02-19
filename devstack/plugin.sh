dd2024ee95f4e6b81b48466ab10d5b69ff14e719
Sean Mooney <work@seanmooney.info>
2020-02-19 23:46:47 +0000

    simplify devstack plugin
    This change replaces the nested if used
    in plugin.sh with nested case statements for
    greater readablity.
    
    This change removes an unnecessary check to determine
    if services are enabled. By default all cyborg
    service are enabled by the plugin. Since the plugin is
    not loaded unless the user specifies the enable_plugin
    line in there local.conf we can assume they want the plugin
    to run and there for can remove the service check.
    
    This change removes the install_agent function and renames
    install_cyborg_in_controller function to
    check_cyborg_service_deps to better reflect what the function
    does.
    
    Change-Id: Ia5ad1235ae4a5a76bcd498a743ca6be6b3634c5c

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -8,43 +8,43 @@ echo_summary "cyborg devstack plugin.sh called: $1/$2"
 source $DEST/cyborg/devstack/lib/cyborg
 source $DEST/cyborg/devstack/lib/opae
 
-
-if is_service_enabled cyborg-api cyborg-cond || is_service_enabled cyborg-agent; then
-    if [[ "$1" == "stack" ]]; then
-        if [[ "$2" == "pre-install" ]]; then
-            pre_install_agent
-        elif [[ "$2" == "install" ]]; then
-            echo_summary "Installing Cyborg"
-            install_agent
-            install_cyborg
-        elif [[ "$2" == "post-config" ]]; then
-        # stack/post-config - Called after the layer 0 and 2 services have been
-        # configured. All configuration files for enabled services should exist
-        # at this point.
-            echo_summary "Configuring Cyborg"
-            configure_cyborg
-            create_cyborg_accounts
-        elif [[ "$2" == "extra" ]]; then
-        # stack/extra - Called near the end after layer 1 and 2 services have
-        # been started.
-            # Initialize cyborg
-            init_cyborg
-
-            # Start the cyborg API and cyborg taskmgr components
-            echo_summary "Starting Cyborg"
-            start_cyborg
-        fi
-    fi
-
-    if [[ "$1" == "unstack" ]]; then
-    # unstack - Called by unstack.sh before other services are shut down.
+case $1 in
+    "stack")
+        case $2 in
+            "pre-install")
+                pre_install_agent
+                ;;
+            "install")
+                echo_summary "Installing Cyborg"
+                install_cyborg
+                ;;
+            "post-config")
+                # stack/post-config - Called after the layer 0 and 2 services
+                # have been configured. All configuration files for enabled
+                # services should exist at this point.
+                echo_summary "Configuring Cyborg"
+                configure_cyborg
+                create_cyborg_accounts
+                ;;
+            "extra")
+                # stack/extra - Called near the end after layer 1 and 2
+                # services have been started.
+                # Initialize cyborg
+                init_cyborg
+                # Start the cyborg API and cyborg taskmgr components
+                echo_summary "Starting Cyborg"
+                start_cyborg
+                ;;
+        esac
+        ;;
+    "unstack")
+        # unstack - Called by unstack.sh before other services are shut down.
         stop_cyborg
-    fi
-
-    if [[ "$1" == "clean" ]]; then
-    # clean - Called by clean.sh before other services are cleaned, but after
-    # unstack.sh has been called.
+        ;;
+    "clean")
+        # clean - Called by clean.sh before other services are cleaned, but after
+        # unstack.sh has been called.
         cleanup_cyborg
         cleanup_agent
-    fi
-fi
+        ;;
+esac
