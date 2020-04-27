872b36f0e53c80b7fa1d335a97a184a74e554226
Xinran Wang <xin-ran.wang@intel.com>
2020-04-27 11:18:39 +0000

    Fix copy-paste typo
    Change-Id: I8ac4f5b35299bf1f60170ee58aed4d89e48ff3ad

--- cyborg/agent/rpcapi.py
+++ cyborg/agent/rpcapi.py
@@ -13,7 +13,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-"""Client side of the conductor RPC API."""
+"""Client side of the Agent RPC API."""
 
 from oslo_config import cfg
 import oslo_messaging as messaging
